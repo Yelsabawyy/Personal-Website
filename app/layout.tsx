f8d09bd66e6f2eee7343cff1ea578c042c4bbb47
Youssef Elsabawy <yelsabawyy@gmail.com>
2025-09-27 12:22:28 +0300

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Plus } from "lucide-react";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -39,7 +41,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-serif`}
       >
-        <Header />
+        <Header/>
         {children}
         <Footer />
         <SpeedInsights />
